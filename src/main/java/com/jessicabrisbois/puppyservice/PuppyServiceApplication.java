b41ec6e016e785b2b68027edf710c81b96d0b214
Jessica Brisbois <jessica.brisbois@ttu.edu>
2020-05-09 14:39:04 -0500

    Initial commit

---
+++ src/main/java/com/jessicabrisbois/puppyservice/PuppyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jessicabrisbois.puppyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessicabrisbois/puppyservice/PuppyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicabrisbois.puppyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
